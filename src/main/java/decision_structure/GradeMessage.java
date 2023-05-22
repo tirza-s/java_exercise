1b46f2b13796af43f9df6f34825e705e21682ad1
tirzasam07 <83017742+tirzasam07@users.noreply.github.com>
2023-05-22 21:14:57 +0200

    commit

--- src/main/java/decision_structure/GradeMessage.java
+++ src/main/java/decision_structure/GradeMessage.java
@@ -1,4 +1,42 @@
 package decision_structure;
 
+import java.util.Scanner;
+
 public class GradeMessage {
+
+     public static void main(String[] args) {
+
+          System.out.println("Enter your letter grade :");
+
+          Scanner scanner = new Scanner(System.in);
+
+          String grade = scanner.next();
+
+          scanner.close();
+
+          String message;
+
+          switch (grade){
+               case "A" :
+                    message =  "Excellent job!";
+                    break;
+
+               case "B" :
+                    message = "Great Job!";
+                    break;
+
+               case "C" :
+                    message = "Good job!";
+                    break;
+
+               case "D" :
+                    message = "You need to work harder !";
+                    break;
+
+               default:
+                    message = "Ops..Invalid grade";
+                    break;
+          }
+          System.out.println(message);
+     }
 }
